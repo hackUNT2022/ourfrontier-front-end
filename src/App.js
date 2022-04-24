90cd3db695b191f5fa831e233ac65680107a216b
Forehand, Dayton <daytonforehand@my.unt.edu>
2022-04-24 05:37:42 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import './App.css'
 import Glossary from './Pages/Glossary/Glossary'
-import Planet from './Components/Planet/Planet';
 import Navbar from './Components/Nav/Navbar'
-import SolarSystem from './Components/SolarSystem/SolarSystem';
-
-const planet_pictures = [
-  "https://www.nasa.gov/images/content/607196main_messenger_orbit_image20111130_1_4by3_946-710.jpg",
-  "https://www.nasa.gov/sites/default/files/thumbnails/image/imagesvenus20191211venus20191211-16.jpeg",
-  "https://www.nasa.gov/sites/default/files/thumbnails/image/187_1003705_americas_dxm.png",
-  "https://solarsystem.nasa.gov/system/resources/detail_files/683_6453_mars-globe-valles-marineris-enhanced-full2.jpg",
-  "",
-]
+import SolarTour from './Pages/SolarTour';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ISS from './Pages/ISS/ISS';
 
 function App() {
   return (
     <div className="App App__background">
       <Navbar/>
-      {/* Landing goes here */}
-      <SolarSystem />
-      <Planet name={"Mercury"} image={planet_pictures[0] } />
-      <Planet name={"Venus"} image={planet_pictures[1] } />
-      <Planet name={"Earth"} image={planet_pictures[2] } />
-      <Planet name={"Mars"} image={planet_pictures[3] } />
-      <Planet name={"Jupiter"} image={planet_pictures[4] } />
-      <Planet name={"Saturn"} image={planet_pictures[5] } />
-      <Planet name={"Uranus"} image={planet_pictures[6] } />
-      <Planet name={"Neptune"} image={planet_pictures[7] } />
+      <Router>
+        <Routes>
+          <Route path='/' element={<SolarTour />} />
+          <Route path='/glossary' element={<Glossary />} />
+          <Route path='iss' element={<ISS />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Planet/Planet.jsx
+++ src/Components/Planet/Planet.jsx
@@ -1,79 +1,96 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./planet.css"
 import axios from 'axios'
 
 
 const Planet = ({name, image}) => {
+  const [data, setData] = useState([]);
+  const [time, setTime] = useState(Date.now());
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 1000)
+    setIndex(index+1)
+    console.log("hi")
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   const instance = axios.create({
-    baseURL: 'https://solarsystem.nasa.gov',
-    timeout: 1000,
-    headers: {
-      'Access-Control-Allow-Origin': 'origin',
-    }
+    baseURL: 'http://abnormaltech.net',
+    timeout: 2000,
   })
 
-  // const getDistanceBetweenCelestialBodies = async (b1, b2) => {
-  //   function monthConvert(month) {
-  //     if (month === "Jan") return "01"
-  //     if (month === "Feb") return "02"
-  //     if (month === "Mar") return "03"
-  //     if (month === "Apr") return "04"
-  //     if (month === "May") return "05"
-  //     if (month === "Jun") return "06"
-  //     if (month === "Jul") return "07"
-  //     if (month === "Aug") return "08"
-  //     if (month === "Sep") return "09"
-  //     if (month === "Oct") return "10"
-  //     if (month === "Nov") return "11"
-  //     if (month === "Dec") return "12"
+  const getDistanceBetweenCelestialBodies = async (b1, b2) =>  {
+
+    function monthConvert(month) {
+      if (month === "Jan") return "01"
+      if (month === "Feb") return "02"
+      if (month === "Mar") return "03"
+      if (month === "Apr") return "04"
+      if (month === "May") return "05"
+      if (month === "Jun") return "06"
+      if (month === "Jul") return "07"
+      if (month === "Aug") return "08"
+      if (month === "Sep") return "09"
+      if (month === "Oct") return "10"
+      if (month === "Nov") return "11"
+      if (month === "Dec") return "12"
     
-  //   }
+    }
 
-  //   const currentTime = new Date(Date.now())
-  //   const fiveMinTime = new Date(Date.now() + (300*1000))
+    const currentTime = new Date(Date.now())
+    const fiveMinTime = new Date(Date.now() + (300*1000))
 
-  //   const vals1 = currentTime.toUTCString().split(" ");
-  //   const vals2 = fiveMinTime.toUTCString().split(" ");
+    const vals1 = currentTime.toUTCString().split(" ");
+    const vals2 = fiveMinTime.toUTCString().split(" ");
 
-  //   let t1 = `${vals1[3]}-${monthConvert(vals1[2])}-${vals1[1]}T${vals1[4]}`
-  //   t1.slice(0, -1)
-  //   t1.slice(0, -1)
-  //   t1 += "0"
+    let t1 = `${vals1[3]}-${monthConvert(vals1[2])}-${vals1[1]}T${vals1[4]}`
+    t1.slice(0, -1)
+    t1.slice(0, -1)
+    t1 += "0"
 
-  //   let t2 = `${vals2[3]}-${monthConvert(vals2[2])}-${vals2[1]}T${vals2[4]}`
-  //   t2.slice(0, -1)
-  //   t2.slice(0, -1)
-  //   t2 += "0"
+    let t2 = `${vals2[3]}-${monthConvert(vals2[2])}-${vals2[1]}T${vals2[4]}`
+    t2.slice(0, -1)
+    t2.slice(0, -1)
+    t2 += "0"
 
-  //   // https://solarsystem.nasa.gov/spice_data/getRangefromT1/EARTH/NEPTUNE/2022-04-23T23:58:0/2022-04-24T00:05:0/4/
-  //   const route = `/spice_data/getRangefromT1/${b1.toUpperCase()}/${b2.toUpperCase()}/${t1}/${t2}/1/`
+    // abnormaltech.net/get_planet_data?p1=MERCURY&p2=NEPTUNE&t1=2022-04-23T23:58:0&t2=2022-04-24T00:050
+    const route = `/get_planet_data?p1=${b1.toUpperCase()}&p2=${b2.toUpperCase()}&t1=${t1}&t2=${t2}`
 
-  //   instance.get(route, { crossDomain: true })
-  //   .then(function (response) {
-  //     // handle success
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     // handle error
-  //     console.log(error);
-  //   })
+    instance.get(route)
+    .then(function (response) {
+      // handle success
+      setData(...response.data.items)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    
+    console.log(data)
+  }
 
+  useEffect(() => {
+    getDistanceBetweenCelestialBodies(name, "earth")
+  }, [])
 
-  // }
+  useEffect(() => {
+    console.log(data)
+  }, [data])
 
   return (
     <section className='Planet__container' id={name}> 
       <div className='Planet__container-top'>
         <div className='Planet__image-container'>
           <img className='Planet__image' src={image} alt={`The planet ${name}`}/>
-
         </div>
         <div className='Planet__stats-container'>
           <h1>{name}</h1>
           <h3>{`Distance from Earth: ${name}`}</h3>
           <h3>{`Weather: ${name}`}</h3>
-          <h3>{`Distance from the Earth: ${name}`}</h3>
+          <h3>{`Distance from the Earth: ${index} KM`}</h3>
         </div>
       </div>
       <div className='Planet__container-bottom'>
