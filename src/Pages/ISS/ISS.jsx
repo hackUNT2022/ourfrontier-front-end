2060af34e7fa7a7de158d1479b98eef44ce21402
Forehand, Dayton <daytonforehand@my.unt.edu>
2022-04-24 11:21:33 -0500

    ISS distance pinger works

--- src/Pages/ISS/ISS.jsx
+++ src/Pages/ISS/ISS.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ISS.css'
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import axios from 'axios'
+//import { Wrapper, Status } from '@googlemaps/react-wrapper'
+
+
+
 
 const ISS = () => {
-  return (
-    <div className='ISS__container'>
-      <div className="ISS__image_container">
-        <img src='https://www.washingtonpost.com/wp-srv/special/national/nasa-iss/img/ISS-2008.png' alt='The international space station' />
-        <h2>THE INTERNATIONAL SPACE STATION </h2>
-      </div>
-      <div className="ISS__info_container">
-        <div className="ISS__info_content">
-          <h2>Here are some cool things about the ISS</h2>
-        </div>
-      </div>
-    </div>
-  )
+  const [distance, setDistance] = useState(0);
+    const instance = axios.create({
+        baseURL: 'https://abnormaltech.net',
+        timeout: 5000,
+    })
+
+    const route = "/iss"
+    const interval = setInterval(() => { 
+        handleSubmit();
+    }, 5000)
+
+    useEffect(() => {
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+    const handleSubmit = async () => {
+
+        instance.get(route)
+            .then(function (response) {
+                // display as dist to iss
+                setDistance(response.data)
+                console.log(response)
+            })
+            .catch(function (error) {
+                // handle error
+                console.warn("caught error -->", error);
+            })
+    }
+
+    useEffect(() => {
+      console.log(distance)
+    }, [distance])
+
+    return (
+        <>
+            <div className='ISS__container'>
+                <div className="ISS__image_container">
+                    <img src='https://www.washingtonpost.com/wp-srv/special/national/nasa-iss/img/ISS-2008.png' alt='The international space station' />
+                    <h2>THE INTERNATIONAL SPACE STATION </h2>
+                </div>
+                <div className="ISS__info_container">
+                    <div className="ISS__info_content">
+                        <h2>This is your distance from the ISS</h2>
+                        <h3>{(distance) ? `${distance.toFixed(2)} km` : "Loading"}</h3>
+                    </div>
+                </div>
+            </div>
+        </>)
 }
+
 
 export default ISS
