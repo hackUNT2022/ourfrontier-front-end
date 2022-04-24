6a2673b7c918451a131b3f865b8be63c514c22b3
e.kobedevon <e.kobedevon@gmail.com>
2022-04-23 23:22:08 -0500

    Added solar system landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Glossary from './Pages/Glossary/Glossary'
 import Planet from './Components/Planet/Planet';
 import Navbar from './Components/Nav/Navbar'
+import SolarSystem from './Components/SolarSystem/SolarSystem';
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App App__background">
       <Navbar/>
+      <SolarSystem/>
     </div>
   );
 }
